3425591e8065c2aaa7b978a5bfcae11a16b62116
NeutrinoZh <neutrinozh@gmail.com>
2024-01-06 09:11:48 +0200

    client-host connections

--- Assets/DepthOfDarkness/Scripts/Services/SceneManagement/SceneManagement.cs
+++ Assets/DepthOfDarkness/Scripts/Services/SceneManagement/SceneManagement.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 namespace DD {
     public enum SceneList {
@@ -10,7 +9,7 @@ namespace DD {
         GAME
     }
     
-    public sealed class SceneManagement {
+    public sealed class SceneManagement : NetworkBehaviour {
         private const string LOADING_SCREEN = "LOADING_SCREEN";
 
         private bool mLoading;
@@ -20,14 +19,15 @@ namespace DD {
 
         public GameObservable GameObservable { get; set; }
 
+        private void Awake() {
+            DontDestroyOnLoad(this);
+        }
+
         public void LoadScene(SceneList _scene) {
             if (mLoading)
                 return;
             mLoading = true;
             mToLoadScene = _scene;
-                
-          //  NetworkManager.Singleton.SceneManager.LoadScene(LOADING_SCREEN, LoadSceneMode.Additive);
-          //  NetworkManager.Singleton.SceneManager.OnLoadComplete += OnLoadingScreenLoaded;
         
             var proccess = SceneManager.LoadSceneAsync(LOADING_SCREEN, LoadSceneMode.Single);
             proccess.completed += OnLoadingScreenLoaded;
@@ -55,20 +55,23 @@ namespace DD {
         }
 
         private void OnSceneLoaded(ulong _clientId, string _sceneName, LoadSceneMode _loadSceneMode) {
-            if (_clientId != 0)
+            if (_clientId == 0)
+                NetworkManager.Singleton.SceneManager.UnloadScene(SceneManager.GetSceneByName(LOADING_SCREEN));
+            ConnectClientRpc(_clientId);
+        } 
+
+        [ClientRpc] private void ConnectClientRpc(ulong _clientId) {
+            if (NetworkManager.Singleton.LocalClientId != _clientId)
                 return;
-            
+
+            mLoading = false;
+
             foreach (var service in mPreloadServices)
                 service.Execute();
 
-            mLoading = false;
-            NetworkManager.Singleton.SceneManager.UnloadScene(SceneManager.GetSceneByName(LOADING_SCREEN));
-            
             if (GameObservable != null)
                 GameObservable.Run();
-        } 
-
-        // Preload Service Management  
+        }
 
         public void AddPreloadService(IPreloadService _service) {
             mPreloadServices.Add(_service);
--- Assets/DepthOfDarkness/Scripts/Services/SceneManagement/SceneManagementInstaller.cs
+++ Assets/DepthOfDarkness/Scripts/Services/SceneManagement/SceneManagementInstaller.cs
@@ -1,9 +1,12 @@
+using UnityEngine;
 using Zenject;
 
 namespace DD {
     public class SceneManagementInstaller : MonoInstaller {
+        [SerializeField] private SceneManagement mSceneManagement; 
         public override void InstallBindings() {
-            Container.Bind<SceneManagement>().FromNew().AsSingle();
+            var instance = Instantiate(mSceneManagement);
+            Container.Bind<SceneManagement>().FromInstance(instance).AsSingle();
         }
     }
 }
